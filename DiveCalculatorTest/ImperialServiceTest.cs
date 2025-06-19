6d2968de9aee2817ffd59fe63886fdbdb5c2e9eb
Andrew Bielecki <ambielecki@gmail.com>
2025-06-19 14:11:29 -0400

    Divecalculator :: add unit test

---
+++ DiveCalculatorTest/ImperialServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using DiveCalculator.Services.DiveCalculator;
+
+namespace DiveCalculatorTest;
+
+public class ImperialServiceTest
+{
+    [Theory]
+    [InlineData(60, 30, "L")]
+    [InlineData(140, 30, null)] // depth exists, over NDL
+    [InlineData(30, 60, "L")]
+    [InlineData(70, 13, "D")] // exact match
+    [InlineData(150, 1, null)] // out of rec diving limit
+    [InlineData(90, 25, "Q")] // NDL
+    public void GetPressureGroupTest(int depth, int minutes, string? expected) {
+        var diveCalculator = new ImperialDiveCalculator();
+        
+        var pressureGroup = diveCalculator.GetPressureGroup(depth, minutes);
+        Assert.Equal(expected, pressureGroup);
+    }
+}
