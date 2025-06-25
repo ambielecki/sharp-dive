cfc00ca9ab631b4c0a60858686e0b9f46865bdcc
= <=>
2025-06-25 14:13:35 -0400

    Breaking out some service

---
+++ DiveCalculator/Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,28 @@
+﻿using DiveCalculator.Data;
+using DiveCalculator.Services.DiveCalculator;
+using DiveCalculator.Services.Token;
+using Microsoft.EntityFrameworkCore;
+
+namespace DiveCalculator.Extensions;
+
+public static class ApplicationServiceExtensions
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        var serverVersion = new MariaDbServerVersion(new Version("10.6.16"));
+        var connectionString = configuration.GetConnectionString("DefaultConnection");
+        
+        services.AddControllers();
+        services.AddDbContext<DataContext>(opt =>
+        {
+            opt.UseMySql(connectionString, serverVersion);
+        });
+        // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+        services.AddOpenApi();
+
+        services.AddScoped<IDiveCalculator, ImperialDiveCalculator>();
+        services.AddScoped<ITokenService, TokenService>();
+        
+        return services;
+    }
+}
--- DiveCalculator/Program.cs
+++ DiveCalculator/Program.cs
@@ -1,38 +1,10 @@
-using System.Text;
-using DiveCalculator.Data;
-using DiveCalculator.Services.DiveCalculator;
-using DiveCalculator.Services.Token;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
+using DiveCalculator.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
-var serverVersion = new MariaDbServerVersion(new Version("10.6.16"));
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
 // Add services to the container.
-
-builder.Services.AddControllers();
-builder.Services.AddDbContext<DataContext>(opt =>
-{
-    opt.UseMySql(connectionString, serverVersion);
-});
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
-
-builder.Services.AddScoped<IDiveCalculator, ImperialDiveCalculator>();
-builder.Services.AddScoped<ITokenService, TokenService>();
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer(options =>
-    {
-        var tokenKey = builder.Configuration["TokenKey"] ?? throw new Exception("TokenKey not found in config");
-        options.TokenValidationParameters = new TokenValidationParameters{
-            ValidateIssuerSigningKey = true,
-            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(tokenKey)),
-            ValidateIssuer = false,
-            ValidateAudience = false,
-        };
-    });
+builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddIdentityServices(builder.Configuration);
 
 var app = builder.Build();
 
